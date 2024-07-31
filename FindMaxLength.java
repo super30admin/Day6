5176d0e285e08ce08630fa85522d47ae6149697d
Karan Shah <karanshah1561998@gmail.com>
2024-07-31 18:32:03 -0400

    Done Hashing-2|S30_HW6_20240730

---
+++ FindMaxLength.java
@@ -0,0 +1,35 @@
+// Problem 49. Group Anagrams
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+import java.util.HashMap;
+
+class FindMaxLength {
+    public int findMaxLength(int[] nums) {
+        // null
+        if (nums == null) {
+            return 0;
+        }
+        int max = 0;
+        int rsum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                rsum--;
+            } else {
+                rsum++;
+            }
+
+            if (map.containsKey(rsum)) {
+                max = Math.max(max, i - map.get(rsum));
+            } else {
+                map.put(rsum, i);
+            }
+        }
+        return max;
+    }
+}
---
+++ SubArraySum.java
@@ -0,0 +1,28 @@
+// Problem 49. Group Anagrams
+// Time Complexity : O(n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+// Your code here along with comments explaining your approach
+import java.util.HashMap;
+
+class SubArraySum {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        int rsum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1); // Initialize with sum 0 having one occurrence
+
+        for(int i = 0; i < nums.length; i++) {
+            rsum += nums[i]; // Running sum
+            // Check if there is a subarray sum that equals to k
+            if(map.containsKey(rsum - k)) {
+                count += map.get(rsum - k);
+            }
+            map.put(rsum, map.getOrDefault(rsum, 0) + 1);
+        }
+
+        return count;
+    }
+}
