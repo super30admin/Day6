7abe815fb2609f6e57683cf0cc0522d9700628cd
PKULK2 <48229592+PKULK2@users.noreply.github.com>
2024-08-01 13:59:58 -0400

    Completed Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findMaxLength(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1); // Initialize the map with sum 0 at index -1
+        
+        int maxLength = 0;
+        int sum = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i] == 0 ? -1 : 1;
+            
+            if (map.containsKey(sum)) {
+                maxLength = Math.max(maxLength, i - map.get(sum));
+            } else {
+                map.put(sum, i);
+            }
+        }
+        
+        return maxLength;
+    }
+}
