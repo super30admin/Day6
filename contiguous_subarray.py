901b2730d468261f7b7b9feb61e9adb0bd3c38e4
RajBhowmik <bhowmick.raj10@gmail.com>
2024-08-03 14:48:28 -0700

    contiguous_subbarray

--- contiguous_subarray.py
+++ contiguous_subarray.py
@@ -1,3 +1,25 @@
-nums = [1,2,3]
-for i in nums:
-    print(index(i))
+# TC: O(n) SC: O(n)
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return 0
+        hMap={}
+        sum1=0
+        max1= 0
+        hMap[0]= -1       
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                sum1 -=1
+            else:
+                sum1+=1
+            if sum1 not in hMap:
+                    hMap[sum1] = i
+            else:
+                max1 = max(max1, (i - hMap[sum1]))
+        return max1
+
+
+
+    
+        
