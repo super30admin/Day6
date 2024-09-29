5a61ccd67e093a793db4f8cf4f05b73a9cdccb7d
Ruzan Pithawala <36655798+RuzanPithawala@users.noreply.github.com>
2024-09-29 03:36:31 -0400

    Update subarray sum k.java

--- subarray sum k.java
+++ subarray sum k.java
@@ -1,21 +1,17 @@
 class Solution {
-    public int coinChange(int[] coins, int amount) {
-        if(amount ==0) return 0;
-        if(coins==null || coins.length==0) return -1;
-        int m=coins.length;
-        int n=amount;
-        int[][] dp= new int [m+1][n+1];
+    public int subarraySum(int[] nums, int k) {
+        int rsum=0,count=0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0,1);
 
-        for(int j=1;j<dp[0].length;j++){
-            dp[0][j]=amount+1;
-        }
-        for(int i=1;i<dp.length;i++){
-            for(int j=1;j<dp[0].length;j++){
-                if(coins[i-1]>j) dp[i][j]=dp[i-1][j];
-                else dp[i][j]=Math.min(dp[i-1][j],1 + dp[i][j-coins[i-1]]);
+        for(int i=0;i<nums.length;i++){
+            rsum +=nums[i];
+            if(map.containsKey(rsum-k)){
+                count+=map.get(rsum-k);
             }
+            map.put(rsum,map.getOrDefault(rsum,0)+1);
+             
         }
-        if(dp[m][n]>=amount+1) return -1;
-        return dp[m][n];
+        return count;
     }
 }
