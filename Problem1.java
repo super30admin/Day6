eb3ecd42f6bedd8bf73c1cd2d2295ca243e2bad7
kurian <openfeather@hotmail.com>
2024-10-29 17:06:53 -0400

    hashing2 solved

--- Problem1.java
+++ Problem1.java
@@ -1,4 +1,25 @@
 package PACKAGE_NAME;
 
 public class Problem1 {
+    public int subarraySum(int[] nums, int k) {
+
+        Map<Integer,Integer> freq = new HashMap<>();
+        freq.put(0,1);
+        int sum = 0;
+        int rsum = 0;
+        for(int i=0;i<nums.length;i++){
+
+            rsum+=nums[i];
+            if(freq.containsKey(rsum-k)){
+                sum+=freq.get(rsum-k);
+            }
+            freq.put(rsum,freq.getOrDefault(rsum,0)+1);
+
+
+        }
+
+
+        return sum;
+
+    }
 }
