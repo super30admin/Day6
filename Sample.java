20f9e98cf4f6e9e29d8e467a18911e762018e415
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-01 19:42:06 -0800

    longest palindrome Sample.java

--- Sample.java
+++ Sample.java
@@ -5,3 +5,28 @@
 
 
 // Your code here along with comments explaining your approach
+
+
+// Hashing longest palindrome
+
+// Time complexity:O(n)
+// Space complexity: constant time
+class Solution {
+    public int longestPalindrome(String s) {
+
+        int max=0;
+        Map<Character, Integer> count = new HashMap<Character, Integer>();
+        for (int i =0;i<s.length();i++) {
+            if (!count.containsKey(s.charAt(i)))  {
+                count.put(s.charAt(i), 1);
+            } else {
+                max=max+2;
+                count.remove(s.charAt(i));
+            }
+        }
+        if (!count.isEmpty()) {
+            max = max +1;
+        }
+        return max;
+    }
+}
