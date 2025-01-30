d77e5095da1f50e177cae990be0fc6511674242d
bhavana4992 <32822224+bhavana4992@users.noreply.github.com>
2025-01-29 23:48:12 -0600

    Update LongestContArray.java

--- LongestContArray.java
+++ LongestContArray.java
@@ -1,7 +1,7 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : 0(N)
+// Space Complexity : 0(N)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
 
 
 // Your code here along with comments explaining your approach
