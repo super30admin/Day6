316a9cda43f1cd3b8d73b1325086530473259cdb
bhavana4992 <32822224+bhavana4992@users.noreply.github.com>
2025-01-29 23:47:14 -0600

    Update and rename Sample.java to LongestContArray.java

---
+++ LongestContArray.java
@@ -0,0 +1,40 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+
+class LongestContArray {
+    public int findMaxLength(int[] nums) {
+
+        int runningSum=0;
+        int n=nums.length;
+        int ans=0;
+        HashMap<Integer,Integer> hmap = new HashMap<>();
+        hmap.put(0,-1);//dummy arr to catch at index0
+        for(int i=0;i<n;i++){
+
+            if(nums[i]==0){
+                runningSum--;
+
+            }else{
+
+                 runningSum++;
+
+            }
+         if(hmap.containsKey(runningSum)){
+            ans=Math.max(ans,i-hmap.get(runningSum));
+
+         }else{
+
+            hmap.put(runningSum,i);
+         }
+
+        }
+          return ans;
+  
+    }
+}
